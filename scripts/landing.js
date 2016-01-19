19aeb081a4c2de15ca0ba57217899181d940691a
Margaret Babiarz <sspole@gmail.com>
2016-01-19 14:58:38 -0700

    checkpoint 29 good

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,24 @@
-var pointsArray = document.getElementsByClassName("point");
-
-var revealPoints = function(points) {
-  points.style.opacity = 1;
-  points.style.transform = "scaleX(1) translateY(0)";
-  points.style.msTransform = "scaleX(1) translateY(0)";
-  points.style.WebkitTransform = "scaleX(1) tanslateY(0)";
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: "scaleX(1) translateY(0)"
+    });
+  };
+  $.each($('.point'), revealPoint);
 };
 
-var animatePoints = function(points) {
-  forEach(points, revealPoints);
-};
-
-window.onload = function() {
+$(window).load(function() {
   // Automatically animate on tall screens
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+  if ($(window).height() > 950) {
+    animatePoints();
   }
+  
   // Animate on scroll
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight + 200;
-  window.addEventListener('scroll', function(event) {
-    if (document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  var scrollDistance = $('.selling-points').offset().top -    $(window).height() + 200;
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
   });
-}
+});
