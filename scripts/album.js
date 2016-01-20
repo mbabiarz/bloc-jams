5dbeb0b6e227c1dd88d06fbf3c8e84a46a2f8f36
Margaret Babiarz <sspole@gmail.com>
2016-01-19 17:09:16 -0700

    refactor collection and album to jquery

--- scripts/album.js
+++ scripts/album.js
@@ -42,11 +42,11 @@ var albumMarmoset = {
 };
 
 // Select elements to populate dynamically
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+  var $albumTitle = $('.album-view-title');
+  var $albumArtist = $('.album-view-artist');
+  var $albumReleaseInfo = $('.album-view-release-info');
+  var $albumImage = $('.album-cover-art');
+  var $albumSongList = $('.album-view-song-list');
 
 // Song table rows template
 var createSongRow = function(songNumber, songName, songLength) {
@@ -56,20 +56,21 @@ var createSongRow = function(songNumber, songName, songLength) {
     + ' <td class="song-item-title">' + songName + '</td>'
     + ' <td class="song-item-duration">' + songLength + '</td>'
     + '</tr>';
-  return template;
+  return $(template);
 };
 
 var setCurrentAlbum = function(album) {
-  // Assign values to album info
-  albumTitle.firstChild.nodeValue = album.name;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.innerHTML = album.year + ' ' + album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
+  $albumTitle.text(album.name);
+  $albumArtist.text(album.artist);
+  $albumReleaseInfo.text(album.label);
+  $albumImage.attr('src', album.albumArtUrl);
   // Clear content of the album song list container
-  albumSongList.innerHTML = '';
+  $albumSongList.empty();
   // Build list of songs from album JS object
   for (i = 0; i < album.songs.length; i++) {
-    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length); 
+    //albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length); 
+    var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+    $albumSongList.append($newRow);
   }
 };
 
