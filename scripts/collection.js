e6454565500259db906aaaaedf281af14b4dec74
Margaret Babiarz <sspole@gmail.com>
2016-01-26 11:37:31 -0700

    add fix for remote path resolution

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,12 +1,17 @@
+var loc = document.location,
+    isRemote = loc.hostname != '127.0.0.1' && loc.hostname != 'localhost',
+    path = (isRemote) ? '/' + loc.pathname.split('/')[1] : '';
+console.log('path', path);
+
 var buildCollectionItemTemplate = function() {
   var template = 
   '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
   + '    <p>'
-  + '      <a class="album-name" href="/album.html"> The Colors </a>'
+  + '      <a class="album-name" href="'+ path +'/album.html"> The Colors </a>'
   + '      <br/>'
-  + '      <a href="/album.html"> Pablo Picasso </a>'
+  + '      <a href="' + path + '/album.html"> Pablo Picasso </a>'
   + '      <br/>'
   + '      X songs'
   + '      <br/>'
